c7f11fe2fffd6227b5489285fa0c93873da82f16
Eugene <eugene.ratkevich@gmail.com>
2017-11-21 13:08:34 +0300

    Create abstract class BaseFormRequest

--- app/Http/Requests/BaseFormRequest.php
+++ app/Http/Requests/BaseFormRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
-class BaseFormRequest extends FormRequest
+abstract class BaseFormRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
